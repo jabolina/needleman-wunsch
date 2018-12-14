582c9e6ac71c8817c2c228028004da9f9194ba34
jabolina <jose.bolina@neppoadm.com.br>
2018-12-13 22:36:18 -0200

    read nucleotides sequences from file

--- src/alignment.erl
+++ src/alignment.erl
@@ -1,17 +1,38 @@
 -module(alignment).
--export([compare/0]).
+-export([compare_input/0, compare_file/1]).
 
--spec compare() -> matrix:matrix().
-compare() ->
+-spec compare_input() -> matrix:matrix().
+compare_input() ->
     {ok, [SequenceA]} = io:fread("First DNA sequence: ", "~s"),
     {ok, [SequenceB]} = io:fread("Second DNA sequence: ", "~s"),
     needleman_wunsch(SequenceA, SequenceB).
+
+-spec compare_file(string()) -> matrix:matrix().
+compare_file(FileName) ->
+    case read_file(FileName) of
+        [] -> matrix:create_matrix(1, 1);
+        [SeqA, SeqB] -> needleman_wunsch(SeqA, SeqB)
+    end.
 
 -spec similarity(string(), string()) -> integer().
 similarity(FirstGene, SecondGene) ->
     case (string:equal(FirstGene, SecondGene, true)) of
         true -> 1;
         false -> -1
+    end.
+
+-spec read_file(string()) -> list().
+read_file(FileName) ->
+    case file:open(FileName, [read]) of
+        {ok, Device} -> read_lines(Device);
+        _ -> []
+    end.
+
+-spec read_lines(any()) -> list().
+read_lines(Device) ->
+    case file:read_line(Device) of
+        {ok, Data} -> [string:trim(string:replace(Data, "\n", "")) | read_lines(Device)];
+        eof -> []
     end.
 
 -spec needleman_wunsch(string(), string()) -> matrix:matrix().
@@ -21,12 +42,12 @@ needleman_wunsch(SequenceA, SequenceB) ->
         string_iterate(SequenceB,
         fun(B, SIdx) ->
                 matrix:set_element(FIdx, SIdx,
-                max(matrix:get_element(FIdx - 1, SIdx - 1, B)
-                    + similarity(string:substr(SequenceA, FIdx - 1, 1),
-                                string:substr(SequenceB, SIdx - 1, 1)),
-                    max(matrix:get_element(FIdx - 1, SIdx, B) + (-1.0),
-                        matrix:get_element(FIdx, SIdx - 1, B) + (-1.0))),
-                B)
+                    max(matrix:get_element(FIdx - 1, SIdx - 1, B)
+                        + similarity(string:substr(SequenceA, FIdx - 1, 1),
+                                     string:substr(SequenceB, SIdx - 1, 1)),
+                        max(matrix:get_element(FIdx - 1, SIdx, B) + (-1.0),
+                            matrix:get_element(FIdx, SIdx - 1, B) + (-1.0))),
+                    B)
         end, A, 2)
     end, matrix:create_matrix(string:length(SequenceA) + 1, string:length(SequenceB) + 1), 2).
 
